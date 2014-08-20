6f409cc274c6a6efdf51ad5ae19e702eade689e8
Eli Rodgers-Melnick <er432@cornell.edu>
2014-08-20 12:54:19 -0400

    Added get_affected_ref_bases function to VCF_parser

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = ['genomfart',
            ]
 setup(
     name='genomfart',
-    version='0.4',
+    version='0.5',
     author='Eli Rodgers-Melnick',
     author_email='er432@cornell.edu',
     description='A Genomics package for Python',
