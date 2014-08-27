ca25de2b92e82f0d0d13d31b00e44c1ae0894674
Eli Rodgers-Melnick <er432@cornell.edu>
2014-08-27 11:21:08 -0400

    Updated version info

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = ['genomfart',
            ]
 setup(
     name='genomfart',
-    version='0.6',
+    version='0.7',
     author='Eli Rodgers-Melnick',
     author_email='er432@cornell.edu',
     description='A Genomics package for Python',
